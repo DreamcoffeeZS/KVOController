d4703fd99581d9ec07043378eec5feda9ef6af5d
zhoushuai <zhoushuai@micous.com>
2020-04-24 18:56:40 +0800

    学习源码

--- FBKVOController/NSObject+FBKVOController.h
+++ FBKVOController/NSObject+FBKVOController.h
@@ -26,6 +26,7 @@ NS_ASSUME_NONNULL_BEGIN
  @return FBKVOController associated with this object, creating one if necessary
  @discussion This makes it convenient to simply create and forget a FBKVOController, and when this object gets dealloc'd, so will the associated controller and the observation info.
  */
+//适合正常用法：
 //通过AssociatedObject给NSObject提供一个retain类型的FBKVOController
 @property (nonatomic, strong) FBKVOController *KVOController;
 
@@ -36,7 +37,23 @@ NS_ASSUME_NONNULL_BEGIN
  Use this version when a strong reference between controller and observed object would create a retain cycle.
  When not retaining observed objects, special care must be taken to remove observation info prior to deallocation of the observed object.
  */
+//适合[self observe self]的情况：
 //通过AssociatedObject给NSObject提供一个非retain类型的FBKVOController
+//需要手动调用removeObserver或者self.KVOControllerNonRetaining unobserve:
+//另外，这种self observe self实在没有必要用kvo，直接在setter方法中处理即可嘛；
+
+/*
+//注意：要在dealloc方法走之前 remove observation，didMoveToSuperview方法正好合适
+ - (void)didMoveToSuperview
+ {
+     [super didMoveToSuperview];
+     if (self.superview) {
+         [self.KVOControllerNonRetaining observe:self keyPath: ...]
+     }else {
+         [self.KVOControllerNonRetaining unobserve:self];
+     }
+ }
+*/
 @property (nonatomic, strong) FBKVOController *KVOControllerNonRetaining;
 
 @end
